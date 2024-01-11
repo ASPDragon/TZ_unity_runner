42d077aa2036ae9bb993a6c5bf7617b905ee7dff
Sergei <aspdrago@gmail.com>
2024-01-11 16:59:00 +0800

    The project has been updated

--- Assets/__Scripts/Collisions.cs
+++ Assets/__Scripts/Collisions.cs
@@ -6,17 +6,18 @@ public class Collisions : MonoBehaviour {
     public Material[] materials;
     Renderer rend;
     private GameObject lastTriggeredGo = null;
+    PlayerMovement movement;
     // Start is called before the first frame update
     void Start() {
         rend = GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = materials[score];
+        movement = GetComponent<PlayerMovement>();
     }
 
     void OnCollisionEnter(Collision col) {
         Transform rootT = col.gameObject.transform.root;
         GameObject go = rootT.gameObject;
-        PathFollower follower = GetComponent<PathFollower>();
 
         if (go == lastTriggeredGo)
             return;
@@ -31,19 +32,19 @@ public class Collisions : MonoBehaviour {
                 }
                 break;
             case "Finish":
-                follower.enabled = false;
+                movement.enabled = false;
                 break;
         }
     }
 
     void OnTriggerEnter(Collider col) {
-        PathFollower follower = GetComponent<PathFollower>();
+        PlayerMovement movement = GetComponent<PlayerMovement>();
         if (col.tag == "Obstacle") {
             if (score > 0) {
                 rend.sharedMaterial = materials[--score];
             }
             else {
-                follower.enabled = false;
+                movement.enabled = false;
             }
         }
     }
